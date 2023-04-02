99c64248dcafbd55e9124cd56be6642507d03076
Brandon Morgan <brandmorgan@gmail.com>
2023-04-02 15:49:07 -0700

    add possible answers in html, wired up JS

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
-window.addEventListener("load", function() {
-const form = document.querySelector("form");
-form.addEventListener("sumbit", surveySubmit)
+window.addEventListener("load", function () {
+  const form = document.getElementById("surveyDrop");
+  form.addEventListener("submit", surveySubmit);
 });
 
-function surveySubmit (e) {
-e.preventDefault();
-
+function surveySubmit(e) {
+  e.preventDefault();
+  console.log("form got submitted");
 }
-
-;
+function onFormSubmit() {
+  console.log("anything");
+}
