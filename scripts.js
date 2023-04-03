973a63586cd629a16bd39a986bce383ab75f2806
Brandon Morgan <brandmorgan@gmail.com>
2023-04-02 19:11:18 -0700

    wrote language selection logic, assign elements to html/js

--- scripts.js
+++ scripts.js
@@ -5,8 +5,25 @@ window.addEventListener("load", function () {
 
 function surveySubmit(e) {
   e.preventDefault();
-  console.log("form got submitted");
-}
-function onFormSubmit() {
-  console.log("anything");
+
+  const role = document.getElementById("role").value;
+  const color = document.getElementById("color").value;
+  const animal = document.getElementById("animal").value;
+  const car = document.getElementById("car").value;
+  const turtle = document.getElementById("turtle").value;
+
+  let language;
+  if (role === "Full Time Job") {
+    if (animal === "dog") {
+      language = "HTML";
+    } else {
+      language = "CSS";
+    }
+  } else {
+    language = "JS";
+  }
+  console.log(language);
+  htmlEl = document.getElementById("html");
+  cssEl = document.getElementById("css");
+  jsEl = document.getElementById("js");
 }
